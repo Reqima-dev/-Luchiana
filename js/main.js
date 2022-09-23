8430d06a50101e1e4dcc974e818f1a63817a5624
Adlou KONDO <104492843+Reqima-dev@users.noreply.github.com>
2022-09-23 15:03:42 +0100

    mie à jour du vendredi

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+
+
 const data = [
     {
         slideImg: `<img id="slideImg" src="image/Slide1.jpg">`,
         titles: `<h1>Nos Produits </h1>`,
         sous_titles: `<p>   sont fabriqués avec des huiles et beurres végétaux non comédogènes alors dites bye-bye
         aux comédons.</p>`,
-        btnVoirplus : `<a href="#">voir plus</a>`
+        btnVoirplus: `<a href="#">voir plus</a>`
     },
     {
         slideImg: `<img id="slideImg" src="image/Slide2.jpg">`,
         titles: `<h1>Le Saviez-vous ? </h1>`,
         sous_titles: `<p>Nous disposons d'une GAMME complète de produits de soins corporels conçue pour révéler la beauté de la peau noire.</p>`,
-        btnVoirplus : `<a href="#">voir plus</a>`
+        btnVoirplus: `<a href="#">voir plus</a>`
     },
 
     {
@@ -18,7 +23,7 @@ const data = [
         titles: `<h1>AllianzNature </h1>`,
         sous_titles: `<p>   Première marque Béninoise de cosmétiques naturels et éthiques avec des savons artisanaux aux formes
         irrégulières et atypiques dotées d'une légère touche artistique.</p>`,
-        btnVoirplus : `<a href="#">voir plus</a>`
+        btnVoirplus: `<a href="#">voir plus</a>`
 
     },
     {
@@ -26,7 +31,7 @@ const data = [
         titles: `<h1>Nos Produits </h1>`,
         sous_titles: `<p>   sont fabriqués avec des huiles et beurres végétaux non comédogènes alors dites bye-bye
         aux comédons.</p>`,
-        btnVoirplus : `<a href="#">voir plus</a>`
+        btnVoirplus: `<a href="#">voir plus</a>`
     },
 
 ];
@@ -87,6 +92,31 @@ function sliders() {
 
 
 
+//when scroll change navabar 
+
+window.addEventListener('scroll', () => {
+    document.querySelector('header').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+for (let i = 0; i < sortBtn.length; i++) {
+    sortBtn[i].addEventListener('click', function () {
+        for (let j = 0; j < sortBtn.length; j++) {
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
 
 
+        let targetData = this.getAttribute('data-target');
 
+        for (let k = 0; k < sortItem.length; k++) {
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if (sortItem[k].getAttribute('data-item') == targetData || targetData == "all") {
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
